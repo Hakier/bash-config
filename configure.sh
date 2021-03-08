2a41fcb44bf9eef2ccd3444b1646a9dc4125d644
hakier <hakier29a@gmail.com>
2021-03-08 16:56:39 +0100

    ♛ extracted helpers from configure to a separate file

--- configure.sh
+++ configure.sh
@@ -1,34 +1,6 @@
 #!/bin/bash
 
-function getScriptDirectory {
-  cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
-}
-
-function confirm {
-  read -rp "$1 ([y]yes or [N]o): "
-  case $(echo "$REPLY" | tr 'A-Z' 'a-z') in
-    y|yes)  return 0  ;;
-    *)      return 1  ;;
-  esac
-}
-
-function requireFileNotExist {
-  filePath="${1}"
-  message="${2}"
-
-  if [[ ! -e "${filePath}" ]]; then
-    return 0
-  else
-    confirm "${message:="Do you want to remove ${filePath}?"}" &&
-      rm "${filePath}" -v &&
-      return 0 ||
-      return 1
-  fi
-}
-
-function printSeparator {
-  echo '----------'
-}
+source ./helpers.sh
 
 CONFIG_DIR="$(getScriptDirectory)"
 
